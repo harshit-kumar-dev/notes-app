b13205aff3a710f3a472b2e89d654b4c144e5faa
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-04-17 15:47:01 +0530

    corrected the routes

--- assignment-1/src/app.js
+++ assignment-1/src/app.js
@@ -6,4 +6,8 @@ app.use(express.json());
 const noteRoutes  = require('./routes/note.routes');
 app.use('/api/notes', noteRoutes);
 
+app.get('/api/notes', (req, res) => {
+  res.send("Notes working ✅");
+});
+
 module.exports = app;
