13e171a0a3990c2f508e1b4627e8ecf5578f862b
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-04-24 09:43:53 +0530

    update

--- assignment-1/src/index.js
+++ assignment-1/src/index.js
@@ -1,37 +1,3 @@
-// require('dotenv').config();
-
-// const app = require('./notes.route');
-// const connectDB = require('./config/db');
-
-// const PORT = process.env.PORT || 3000;
-
-// const startServer = async () => {
-
-//   app.get("/", (req, res) => {
-//     res.send("API is running 🚀");
-//   });
-
-//   try {
-//     await connectDB(); //  DB first
-//     console.log("ENV CHECK:", process.env.MONGO_URL);
-
-//     app.listen(PORT, () => {
-//       console.log(`Server running at http://localhost:${PORT}`);
-//     });
-
-
-//   } catch (error) {
-//     console.error("Failed to start server:", error.message);
-//   }
-// };
-
-// startServer();
-
-
-
-
-
-
 require('dotenv').config();
 
 const app = require('./notes.route');
@@ -39,17 +5,14 @@ const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 
-// ✅ Root route
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-// ✅ 404 handler (keep at bottom)
 app.use((req, res) => {
   res.status(404).send("Route not found here");
 });
 
-// ✅ Start server properly
 const startServer = async () => {
   try {
     await connectDB();
